f8ab45d36527f6e0c3877a2226da4f4065f0f8d4
educa2ucv <educa2ucv@gmail.com>
2021-01-29 20:50:12 -0400

    Operations is working

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/Button.css';
 
-const Button = ( { value } ) => {
+const Button = ( { value, onClick } ) => {
 
     return (
         <>
             <button 
                 value="value"
                 className="btn"
+                onClick= { onClick }
             >
                 { value }
             </button>  
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/Screen.css';
 
-const Screen = () => {
+const Screen = ({ value }) => {
     return (
 
         <input 
             className="screen" 
             type="text"
+            value= { value }
             readOnly
         />
         
